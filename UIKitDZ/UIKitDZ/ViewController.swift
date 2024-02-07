394f5706f691cd5335c01b53935c79fdde7f3a15
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-02-07 21:50:42 +0500

    Добавил background и приветствующий alert

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -3,9 +3,36 @@
 
 import UIKit
 
-// Описание
+// Главный экран
 class ViewController: UIViewController {
+    let nameLabel = UILabel()
+
+    override func viewDidAppear(_ animated: Bool) {
+        alertGreeting(title: "Пожалуйста, представьтесь", message: "", style: .alert)
+    }
+
     override func viewDidLoad() {
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "Background")
+        backgroundImage.contentMode = .scaleToFill
+        view.insertSubview(backgroundImage, at: 0)
+
+        let nameLabelFrame = CGRect(x: 0, y: 0, width: 375, height: 82)
+        nameLabel.frame = nameLabelFrame
+        nameLabel.text = "Привет "
+        view.addSubview(nameLabel)
+    }
+
+    func alertGreeting(title: String, message: String, style: UIAlertController.Style) {
+        let alertGreeting = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "Готово", style: .default) { _ in
+            let text = alertGreeting.textFields?.first
+        }
+
+        alertGreeting.addTextField()
+        alertGreeting.addAction(action)
+        present(alertGreeting, animated: true, completion: nil)
     }
 }
