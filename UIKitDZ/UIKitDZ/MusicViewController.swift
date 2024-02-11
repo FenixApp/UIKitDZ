d79af364c6dd20a5c85306ea0e744dcf1fb4e9f7
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-02-12 03:25:15 +0500

    Добавил final class

--- UIKitDZ/UIKitDZ/MusicViewController.swift
+++ UIKitDZ/UIKitDZ/MusicViewController.swift
@@ -5,44 +5,46 @@ import AVFoundation
 import UIKit
 
 /// Экран с музыкальной композицией
-class MusicViewController: UIViewController {
+final class MusicViewController: UIViewController {
     // MARK: - IBOutlets
+
     @IBOutlet var playButton: UIButton!
     @IBOutlet var currentValueLabel: UILabel!
     @IBOutlet var musicSlider: UISlider!
     @IBOutlet var musicDescriptionLabel: UILabel!
     @IBOutlet var musicTitleLabel: UILabel!
     @IBOutlet var musicImageView: UIImageView!
-    
+
     private var player = AVAudioPlayer()
     var timer: Timer?
     var music: Music?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         musicImageView.image = UIImage(named: music?.image ?? "nil")
         musicTitleLabel.text = music?.title
         musicDescriptionLabel.text = music?.description
     }
-    
+
     // MARK: - IBAction
-    
+
     @IBAction func playButtonTapped(_ sender: UIButton) {
         playButton.setImage(UIImage(systemName: "stop.circle"), for: .normal)
     }
-    
+
     @IBAction func closeButton(_ sender: UIButton) {
         dismiss(animated: true)
     }
-    
+
     private func setMusic() {
         guard let music = music else { return }
         musicImageView.image = UIImage(named: music.image)
         musicTitleLabel.text = music.title
         musicDescriptionLabel.text = music.description
     }
+
     // MARK: - Private Methods
-    
+
     private func setPlayer() {
         if player.isPlaying {
             playButton.setImage(UIImage(systemName: "stop.circle"), for: .normal)
@@ -50,7 +52,7 @@ class MusicViewController: UIViewController {
         } else {
             playButton.setImage(UIImage(systemName: "play.circle"), for: .normal)
             guard let pathAudio = Bundle.main.path(forResource: music?.nameMusic, ofType: "mp3") else { return }
-            
+
             do {
                 try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: pathAudio))
                 player.play()
--- UIKitDZ/UIKitDZ/PlaylistViewController.swift
+++ UIKitDZ/UIKitDZ/PlaylistViewController.swift
@@ -5,23 +5,35 @@ import AVFoundation
 import UIKit
 
 /// Экран со списком музыкальных композиций
-class PlaylistViewController: UIViewController {
+final class PlaylistViewController: UIViewController {
     // MARK: - IBOutlets
-    
+
     @IBOutlet var twoDescription: UILabel!
     @IBOutlet var twoTitle: UILabel!
     @IBOutlet var twoImage: UIImageView!
     @IBOutlet var oneDescription: UILabel!
     @IBOutlet var oneTitle: UILabel!
     @IBOutlet var oneImage: UIImageView!
-    
+
     // MARK: - Private Properties
-    
-    private var musicOne = Music(image: "Artists", title: "DIARO", description: "On The Way", time: "04:30", nameMusic: "01 On The Way")
-    private var musicTwo = Music(image: "Queen", title: "Demeter", description: "Utopia", time: "03:49", nameMusic: "01 Utopia")
+
+    private var musicOne = Music(
+        image: "Artists",
+        title: "DIARO",
+        description: "On The Way",
+        time: "04:30",
+        nameMusic: "01 On The Way"
+    )
+    private var musicTwo = Music(
+        image: "Queen",
+        title: "Demeter",
+        description: "Utopia",
+        time: "03:49",
+        nameMusic: "01 Utopia"
+    )
 
     // MARK: - Override Methods
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         oneImage.image = UIImage(named: "Artists")
@@ -33,22 +45,40 @@ class PlaylistViewController: UIViewController {
     }
 
     // MARK: - IBAction
-    
+
     @IBAction func showFirstSong(_ sender: Any) {
-        transitionToMusic(music: Music(image: "Artists", title: "DIARO", description: "On The Way", time: "04:30", nameMusic: "01 On The Way"))
+        transitionToMusic(music: Music(
+            image: "Artists",
+            title: "DIARO",
+            description: "On The Way",
+            time: "04:30",
+            nameMusic: "01 On The Way"
+        ))
     }
 
     @IBAction func showTwoSong(_ sender: Any) {
-        transitionToMusic(music: Music(image: "Queen", title: "Demeter", description: "Utopia", time: "03:49", nameMusic: "01 Utopia"))
+        transitionToMusic(music: Music(
+            image: "Queen",
+            title: "Demeter",
+            description: "Utopia",
+            time: "03:49",
+            nameMusic: "01 Utopia"
+        ))
     }
-    
+
     // MARK: - Private Methods
 
     private func transitionToMusic(music: Music) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let musicViewController = storyboard
             .instantiateViewController(withIdentifier: "MusicViewController") as? MusicViewController else { return }
-        musicViewController.music = Music(image: music.image, title: music.title, description: music.description, time: music.time, nameMusic: music.nameMusic)
+        musicViewController.music = Music(
+            image: music.image,
+            title: music.title,
+            description: music.description,
+            time: music.time,
+            nameMusic: music.nameMusic
+        )
 
         musicViewController.modalPresentationStyle = .formSheet
         present(musicViewController, animated: true)
